8ed148d0d1855cd40e14592d094fb85b3a0018f6
Shahid Sabir <sabir.ict@gmail.com>
2020-02-08 12:00:51 +0600

    this is an islamic app

---
+++ IslamicGadget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IslamicGadget
+//
+//  Created by Shahid Sabir on 6/2/20.
+//  Copyright © 2020 Shahid Sabir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Bismillah")
+    }
+
+
+}
+
